bccbd2c4be24cbb220757f546865077af9d98118
William <willplacido40@gmail.com>
2024-07-25 19:29:18 -0300

    update JS

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 
 function calcAge(){
     // let dia = document.getElementById('user_day')
-    let ano = document.getElementById('year_user')
+    let ano = document.getElementById('year_user').value
     let data = new Date()
     let ano_atual = data.getFullYear()
     // let mes = document.getElementsById('month_user')
 
-    let res_year = document.querySelector('#res-Year')
+    let res_year = document.getElementById('res-Year')
 
-    let calcIdade = ano - ano_atual
-    res_year.textContent = calcIdade
+    let calcIdade = ano_atual - ano 
+    res_year.textContent = `${calcIdade}`
 
 
 }

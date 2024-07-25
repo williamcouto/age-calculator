ba23abedcd92d7a5b3e45e2d2324da08b86f51c0
William <willplacido40@gmail.com>
2024-07-25 20:25:55 -0300

    update JS
    criando cálculos das idades

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-
-
 function calcAge(){
     // let dia = document.getElementById('user_day')
-    let ano = document.getElementById('year_user').value
+    let ano = document.getElementById('year_user').value 
+    let mes = document.getElementById('month_user').value
     let data = new Date()
-    let ano_atual = data.getFullYear()
-    // let mes = document.getElementsById('month_user')
+    const ano_Atual = data.getFullYear()
+    const mes_Atual = data.getMonth() + 1
 
+
+    // Calculando o ano de nascimento
     let res_year = document.getElementById('res-Year')
+    let calcAno = ano_Atual - ano 
+    res_year.textContent = `${calcAno} Anos`
 
-    let calcIdade = ano_atual - ano 
-    res_year.textContent = `${calcIdade}`
+    // Calculando os meses
+    let result_Mes = document.getElementById('res-Month')
+    let calcMes = mes_Atual - mes
+    result_Mes.textContent = `${calcMes} Meses`
 
+    // let result_Dia = document.getElementById('res-Day')
+    // let calcDia = 
 
 }

559bf49b109a59f74463e66bea6e3ff64901840c
William <willplacido40@gmail.com>
2024-07-25 15:58:07 -0300

    Update JS

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 
 function calcAge(){
-    
+    let dia = document.getElementById('user_day')
+    let ano = document.getElementById('year_user')
+    let ano_atual = 
+    let mes = document.getElementsById('month_user')
+
+    let calcIdade =  
 }
